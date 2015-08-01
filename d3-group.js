8fef97278ff5f71264f47b47f1a1deaf6a0110b1
Lachlan McDonald <lachlan@radelaide.net>
2015-08-01 20:16:40 +1000

    Create d3-group.js

---
+++ d3-group.js
@@ -0,0 +1,41 @@
+/*!
+ * d3-group
+ * MIT License (MIT)
+ * Copyright (c) 2015 Lachlan McDonald
+ */
+
+d3.group = function() {
+    var data = [],
+        matches = [];
+
+    function group() {}
+
+    group.entries = function(entries) {
+        if (!arguments.length) {
+            return entries;
+        }
+        if (Array.isArray(entries)) {
+            data = entries;
+        }
+        return group;
+    };
+
+    group.on = function(fn) {
+        data = data.filter(function(d) {
+            if (fn.call(null, d) === true) {
+                matches.push(d);
+                return false;
+            } else {
+                return true;
+            }
+        });
+        return group;
+    };
+
+    group.by = function(fn) {
+        data.push(fn.call(null, matches));
+        return data;
+    };
+
+    return group;
+};
